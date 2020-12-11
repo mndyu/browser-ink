6a098d4505c81b6e647152c4cbe8139db5ce5254
mndyu <uygira@gmail.com>
2020-12-11 22:00:42 +0900

    linaria, xo

--- src/ui/popup.tsx
+++ src/ui/popup.tsx
@@ -1,121 +1,136 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 
-// @ts-ignore
-import unified from "unified";
-// @ts-ignore
-import markdown from "remark-parse";
+import {css} from '@linaria/core'
 
-import "../styles/popup.css";
+import unified from 'unified'
+import markdown from 'remark-parse'
 
-// @ts-ignore
-// import browser = require("webextension-polyfill");
+import {Node, Parent} from 'unist'
 
-function linkToTab(link) {
-  const text = link.children[0];
-  return {
-    title: link.title || (text && text.value),
-    url: link.url,
-  };
-}
+import '../styles/popup.css'
+
+// Import browser = require("webextension-polyfill");
 
 const enum Tab {
   Export,
-  Import,
+  Import
 }
 
 const useExportTab = (): [string, number] => {
-  const [code, setCode] = React.useState("");
-  const [winCount, setWinCount] = React.useState(0);
+  const [code, setCode] = React.useState('')
+  const [winCount, setWinCount] = React.useState(0)
 
   React.useEffect(() => {
-    chrome.windows.getAll({ populate: true }, (windows) => {
+    chrome.windows.getAll({populate: true}, (windows) => {
       const windowsCode = windows
         .map((window) => {
-          const windowCode = `## Window ${window.id}`;
+          const windowCode = `## Window ${window.id.toString()}`
           const tabCode = window.tabs
             .map((tab) => {
-              return `- [${tab.title}](${tab.url})`;
+              return `- [${tab.title || ''}](${tab.url || ''})`
             })
-            .join("\n");
-          return `${windowCode}\n${tabCode}`;
+            .join('\n')
+          return `${windowCode}\n${tabCode}`
         })
-        .join("\n\n");
-      setCode(windowsCode);
-      setWinCount(windows.length);
-    });
-  }, []);
+        .join('\n\n')
+      setCode(windowsCode)
+      setWinCount(windows.length)
+    })
+  }, [])
 
-  return [code, winCount];
-};
+  return [code, winCount]
+}
 
 const ExportTab = () => {
-  const [code, winCount] = useExportTab();
+  const [code, winCount] = useExportTab()
   return (
     <div>
       {winCount} windows
       <textarea
         value={code}
-        // onChange={(e) => setCode(e.target.value)}
+        // OnChange={(e) => setCode(e.target.value)}
         rows={15}
         cols={50}
       />
     </div>
-  );
-};
+  )
+}
 
 const ImportTab: React.FC = () => {
-  const [code, setCode] = React.useState("");
+  const [code, setCode] = React.useState('')
   const importWindows = React.useCallback(() => {
-    const tree = unified().use(markdown).parse(code);
-    const lists = tree.children.filter((v) => v.type == "list");
+    const tree = unified().use(markdown).parse(code) as Parent
+    const lists = tree.children.filter((v) => v.type === 'list') as Parent[]
     const tabsInWindows = lists.map((list) =>
       list.children
         .map((listItem) => listItem.children[0].children[0])
         .map((link) => link.url)
-    );
+    )
     tabsInWindows.forEach((tabs) => {
-      chrome.windows.create({ url: tabs });
-    });
-  }, [code]);
+      chrome.windows.create({url: tabs})
+    })
+  }, [code])
 
   return (
     <div>
       <button
         onClick={() => {
-          importWindows();
+          importWindows()
         }}
       >
         open
       </button>
       <textarea
         value={code}
-        onChange={(e) => setCode(e.target.value)}
         rows={15}
         cols={50}
+        onChange={(event) => setCode(event.target.value)}
       />
     </div>
-  );
-};
+  )
+}
 
 const tabs = {
   [Tab.Export]: <ExportTab />,
-  [Tab.Import]: <ImportTab />,
-};
+  [Tab.Import]: <ImportTab />
+}
+
+interface TabButtonProps {
+  onClick: (event: React.MouseEvent) => void
+}
+const TabButton: React.FC<TabButtonProps> = ({children, onClick}) => {
+  return (
+    <div
+      className={css`
+        margin-right: 15px;
+      `}
+      onClick={onClick}
+    >
+      {children}
+    </div>
+  )
+}
 
 const Popup: React.FC = () => {
-  const [tab, setTab] = React.useState(Tab.Export);
+  const [tab, setTab] = React.useState(Tab.Export)
 
   return (
     <div className="popup-padded">
-      <a onClick={() => setTab(Tab.Export)}>export</a>
-      <a onClick={() => setTab(Tab.Import)}>import</a>
+      <div
+        className={css`
+          display: flex;
+        `}
+      >
+        <TabButton onClick={() => setTab(Tab.Export)}>export</TabButton>
+        <TabButton onClick={() => setTab(Tab.Import)}>import</TabButton>
+      </div>
 
       {tabs[tab]}
     </div>
-  );
-};
+  )
+}
 
 // --------------
 
-ReactDOM.render(<Popup />, document.getElementById("root"));
+ReactDOM.render(<Popup />, document.querySelector('#root'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,49 @@
-const path = require('path');
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-    mode: "development",
-    devtool: "inline-source-map",
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
 
-    entry: {
-        content: './src/app/content.ts',
-        background: './src/app/background.ts',
-        popup: './src/ui/popup.tsx',
-    },
+  entry: {
+    content: './src/app/content.ts',
+    background: './src/app/background.ts',
+    popup: './src/ui/popup.tsx'
+  },
 
-    output: {
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: '[name].js'
-    },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js'
+  },
 
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"]
-    },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
 
-    module: {
-        rules: [
-            { test: /\.tsx?$/, loader: "ts-loader" },
-            { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {loader: 'ts-loader'},
+          {
+            loader: '@linaria/webpack-loader',
+            options: {
+              sourceMap: process.env.NODE_ENV !== 'production'
+            }
+          }
         ]
-    },
-};
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles/popup.css'
+    })
+  ]
+}
